9ac7dcc14959c1cedd8e69d85a76847823f5d331
Michael Walsh <10024547@gmit.ie>
2019-11-30 16:59:18 +0000

    addittion of code for scatter plot

--- bostonHouse.py
+++ bostonHouse.py
@@ -36,11 +36,42 @@ print(bos.head())
 
 print(bos.describe())
 
-X=bos.drop('PRICE', axis=1)
-Y=bos['PRICE']
 
-#X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(X,Y,test_size =0.33, random_state = 5)
-#print(X_train.shape)
-#print(X_test.shape)
-#print(Y_train.shape)
-#print(Y_test.shape)
+
+#new part
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+import sklearn.model_selection
+
+X = bos.drop('PRICE', axis = 1)
+Y = bos['PRICE']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.33, random_state = 5)
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
+
+from sklearn.linear_model import LinearRegression
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+
+lm = LinearRegression()
+lm.fit(X_train, Y_train)
+
+Y_pred = lm.predict(X_test)
+
+plt.scatter(Y_test, Y_pred)
+plt.xlabel("Prices: $Y_i$")
+plt.ylabel("Predicted prices: $\hat{Y}_i$")
+plt.title("Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$")
+
+#mse = sklearn.metrics.mean_squared_error(Y_test, Y_pred)
+#print(mse)
+
+
