ec72e2c8582ba08a30b6f1d0a2ac00d963665a8a
Michael Walsh <10024547@gmit.ie>
2019-11-30 15:39:01 +0000

    addittion of code

--- bostonHouse.py
+++ bostonHouse.py
@@ -35,3 +35,12 @@ bos['PRICE'] = boston.target
 print(bos.head())
 
 print(bos.describe())
+
+X=bos.drop('PRICE', axis=1)
+Y=bos['PRICE']
+
+#X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(X,Y,test_size =0.33, random_state = 5)
+#print(X_train.shape)
+#print(X_test.shape)
+#print(Y_train.shape)
+#print(Y_test.shape)
